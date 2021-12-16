0c53b85b8ac939a59bee19898d2f8ffc30348335
MrCheat <40684650+MrCheatEugene@users.noreply.github.com>
2021-12-16 13:07:45 +0300

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -4,4 +4,10 @@ function changeToDarkMode(settings) {
     if (settings && settings.smooth_transition) {
         element.classList.toggle("smooth-transition");
     }
+    
+        const element_t = document.document.getElementById("wrapper");
+    element_t.classList.toggle("dark-mode");
+    if (settings && settings.smooth_transition) {
+        element_t.classList.toggle("smooth-transition");
+    }
 }
